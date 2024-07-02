89d16ec070a4c6e40b69795c0f7ac76517146dd8
DESKTOP-KTIOEGP\Murad Ahmadov <murad.ahmadov@vabiss.com>
2024-07-02 08:30:33 +0400

    - connect DB

---
+++ app.js
@@ -0,0 +1,14 @@
+const express = require('express');
+const morgan = require('morgan');
+
+const app = express();
+
+// Development logging
+if (process.env.NODE_ENV === 'development') {
+  app.use(morgan('dev'));
+}
+
+app.use(express.json({ limit: '10kb' }));
+app.use(express.urlencoded({ extended: true, limit: '10kb' }));
+
+module.exports = app;
