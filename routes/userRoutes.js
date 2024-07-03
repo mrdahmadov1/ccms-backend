8a306ee68726fd7505b5483dd1a02f1653358272
DESKTOP-KTIOEGP\Murad Ahmadov <murad.ahmadov@vabiss.com>
2024-07-03 11:33:43 +0400

    - add checkLogin

--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -7,4 +7,8 @@ router.post('/signup', authController.signup);
 router.post('/login', authController.login);
 router.get('/logout', authController.logout);
 
+router.get('/checkLogin', authController.protect, (req, res) => {
+  res.status(200).json({ status: 'success', data: req.user });
+});
+
 module.exports = router;
