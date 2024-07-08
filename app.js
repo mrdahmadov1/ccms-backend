c7cba7d68574b5d87ac3c53cbf6f850fe70f6674
DESKTOP-KTIOEGP\Murad Ahmadov <murad.ahmadov@vabiss.com>
2024-07-08 13:01:28 +0400

    - add some global middlewares for security and performance

--- app.js
+++ app.js
@@ -3,6 +3,9 @@ const helmet = require('helmet');
 const cors = require('cors');
 const cookieParser = require('cookie-parser');
 const mongoSanitize = require('express-mongo-sanitize');
+const rateLimit = require('express-rate-limit');
+const xss = require('xss-clean');
+const compression = require('compression');
 const morgan = require('morgan');
 const globalErrorHandler = require('./controllers/errorController');
 const complaintRouter = require('./routes/complaintRoutes');
@@ -28,6 +31,14 @@ if (process.env.NODE_ENV === 'development') {
   app.use(morgan('dev'));
 }
 
+// Limit requests from same API
+const limiter = rateLimit({
+  max: 200,
+  windowMs: 60 * 60 * 1000,
+  message: 'Too many requests from this IP, please try again in an hour!',
+});
+app.use('/api', limiter);
+
 // Body parser, reading data from body into req.body
 app.use(express.json({ limit: '10kb' }));
 app.use(express.urlencoded({ extended: true, limit: '10kb' }));
@@ -35,6 +46,11 @@ app.use(cookieParser());
 
 // Data sanitization against NoSQL query injection
 app.use(mongoSanitize());
+
+// Data sanitization against XSS
+app.use(xss());
+
+app.use(compression());
 
 app.use('/api/v1/complaints', complaintRouter);
 app.use('/api/v1/responses', responseRouter);
